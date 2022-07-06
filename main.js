e0d907e6744c61070e4bcb40a2da498e5a9e0fb6
Idorenyin Sunday <85445411+Idsunny13@users.noreply.github.com>
2022-07-06 21:25:56 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,17 @@
-// let display = document.getElementById('inputext');
+let display = document.getElementById('inputext');
 
-// let calculate = (number) => {
-// 	display.value += number;
-// }
+let calculate = (number) => {
+	display.value += number;
+}
 
-// let result = () => {
-// 	try {
-// 		display.value = eval(display.value)
-// 	}
-// 	catch(err) {
-// 		alert('Enter a valid input')
-// 	}
-// }
-
-let word = []
-for(let i = 0; i < word.length; i++) {
-	word = prompt('enter word');
-	if (word !== 'stop') {
-		console.log(word.push[i]);
-	}else {
-		console.log(word);
+let result = () => {
+	try {
+		display.value = eval(display.value)
+	}
+	catch(err) {
+		alert('Enter a valid input')
 	}
 }
+
+
 	
