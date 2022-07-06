57261f6c044060ebee12c8e333826c185bd6560a
Idorenyin Sunday <sundayidorenyin9@gmail.com>
2022-07-06 20:51:53 +0100

    calculator folder added

---
+++ main.js
@@ -0,0 +1,25 @@
+// let display = document.getElementById('inputext');
+
+// let calculate = (number) => {
+// 	display.value += number;
+// }
+
+// let result = () => {
+// 	try {
+// 		display.value = eval(display.value)
+// 	}
+// 	catch(err) {
+// 		alert('Enter a valid input')
+// 	}
+// }
+
+let word = []
+for(let i = 0; i < word.length; i++) {
+	word = prompt('enter word');
+	if (word !== 'stop') {
+		console.log(word.push[i]);
+	}else {
+		console.log(word);
+	}
+}
+	
